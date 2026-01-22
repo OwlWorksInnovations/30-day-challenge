1c63b3523097504c8edc02d03e35cdffd78602da
OwlWorksInnovations <juanpistoris01@gmail.com>
2026-01-22 18:32:39 +0200

    Day Four - Added player health + hud

--- Scripts/cannon_ball.gd
+++ Scripts/cannon_ball.gd
@@ -5,3 +5,7 @@ var cannonSpeed = 0.1
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	position.y -= cannonSpeed
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		SignalBus.player_hit.emit()
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,9 +3,32 @@ extends CharacterBody2D
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @export var fire_rate: float = 1.0
 const CANNON_BALL_PLR = preload("uid://c7e8qtiauda4s")
+const HEALTH = preload("uid://oh335xfxuov7")
+var health_instance: Control
+var sprite1: AnimatedSprite2D
+var sprite2: AnimatedSprite2D
+var sprite3: AnimatedSprite2D
 var playerSpeed: float = 100.0
 var last_direction: Vector2 = Vector2.UP
 var shoot_timer: float = 0.0
+var health: int = 3
+
+# Runs once
+func _ready() -> void:
+	# Set up health UI
+	health_instance = HEALTH.instantiate()
+	
+	get_tree().root.add_child.call_deferred(health_instance)
+	
+	sprite1 = health_instance.get_node("1")
+	sprite2 = health_instance.get_node("2")
+	sprite3 = health_instance.get_node("3")
+	
+	sprite1.frame = 0
+	sprite2.frame = 0
+	sprite3.frame = 0
+	
+	SignalBus.player_hit.connect(update_health)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -25,7 +48,6 @@ func _process(delta: float) -> void:
 		if Input.is_action_just_pressed("shoot"):
 			shoot_cannon()
 			shoot_timer = 0.0
-	
 	move_and_slide()
 
 # Updates the sprite direction
@@ -56,6 +78,26 @@ func shoot_cannon():
 	var cannon_ball_plr = CANNON_BALL_PLR.instantiate()
 	cannon_ball_plr.global_position = self.global_position
 	get_parent().add_child(cannon_ball_plr) 
+
+# Update health
+func update_health():
+	health -= 1
+	match health:
+		0:
+			queue_free()
+			get_tree().reload_current_scene()
+		1:
+			sprite1.frame = 0
+			sprite2.frame = 1
+			sprite3.frame = 1
+		2:
+			sprite1.frame = 0
+			sprite2.frame = 0
+			sprite3.frame = 1
+		3:
+			sprite1.frame = 0
+			sprite2.frame = 0
+			sprite3.frame = 0
 
 # Load next level
 func _on_level_loader_body_entered(body: Node2D) -> void:
